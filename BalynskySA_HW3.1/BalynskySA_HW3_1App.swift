01ff55a70e8e365e73f8f66171f769f637789bac
SBalynsky <bsa89@mail.ru>
2021-03-25 20:15:44 +0300

    Initial Commit

---
+++ BalynskySA_HW3.1/BalynskySA_HW3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  BalynskySA_HW3_1App.swift
+//  BalynskySA_HW3.1
+//
+//  Created by Macbook on 25.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct BalynskySA_HW3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BalynskySA_HW3.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BalynskySA_HW3.1
+//
+//  Created by Macbook on 25.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
